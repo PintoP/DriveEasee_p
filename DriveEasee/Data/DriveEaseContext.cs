535366a39d0e35c281ff1c45f3b9fbee1b27d387
Leandro Ferreira <leandroleitesferreira@hotmail.com>
2024-06-03 17:42:35 +0100

    PT

--- DriveEasee/Data/DriveEaseContext.cs
+++ DriveEasee/Data/DriveEaseContext.cs
@@ -43,7 +43,7 @@ namespace DriveEasee.Data
             if (!optionsBuilder.IsConfigured)
             {
                 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=Pinto;DataBase=DriveEase;Trusted_Connection=true;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-GT5TV8P\\ROOT;DataBase=driveease;Trusted_Connection=true;");
             }
         }
         
